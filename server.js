35bc4173a8501359c5b7bf1bc83bd6722e53cb97
nickyallien6644 <quocvinh12360@gmail.com>
2024-12-22 18:15:08 +0700

    add run local

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
-
+import axios from 'axios'
 
 
   // Init the Express application
@@ -13,23 +13,39 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util.js';
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
   // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+  app.get( "/filteredimage", async ( req, res ) => {
+    try {
+      const image_url = req.query.image_url?.toString();
 
-    /**************************************************************************** */
-
-  //! END @TODO1
+      // Validate the image_url query
+      if (!image_url) {
+        return res.status(400).send({ message: "URL is required" });
+      }
+  
+      // Fetch the image as a buffer using axios
+      const { data: imgBuffer } = await axios.get(image_url, {
+        responseType: "arraybuffer",
+      });
+  
+      // Filter the image
+      const imagePath = await filterImageFromURL(imgBuffer);
+  
+      // Send the filtered image as a response
+      res.status(200).sendFile(imagePath, (errorMessage) => {
+        if (errorMessage) {
+          return res.status(400).send({ message: errorMessage });
+        }
+  
+        // Clean up the local files after response is sent
+        deleteLocalFiles([imagePath]);
+      });
+    } catch (error) {
+      // Handle errors
+      const errorMessage = error.message || "An error occurred";
+      return res.status(422).send({ message: errorMessage });
+    }    
+  });
   
   // Root Endpoint
   // Displays a simple message to the user
